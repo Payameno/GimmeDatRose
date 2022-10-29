5b172a8dc1a1c44568949d93e3fc1b47df0937db
Payam Beigi <beigi.payam@gmail.com>
2022-10-28 23:31:43 -0700

    Write contract

---
+++ contracts/GimmeDatRose.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import 'hardhat/console.sol';
+
+contract GimmeDatRose {
+
+  // Event to emit when a Memo is created
+  event NewMemo(
+    address indexed from,
+    uint256 timestamp,
+    string name,
+    string message
+  );
+
+  //Memo struct
+  struct Memo {
+    address from;
+    uint256 timestamp;
+    string name;
+    string message;
+  }
+}
