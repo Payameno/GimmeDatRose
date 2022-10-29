202436cafb668ed57fcaf5412213944c54b77539
Payam Beigi <beigi.payam@gmail.com>
2022-10-28 23:55:29 -0700

    Add giverose function, add memo to storage

--- contracts/GimmeDatRose.sol
+++ contracts/GimmeDatRose.sol
@@ -32,4 +32,24 @@ contract GimmeDatRose {
     owner = payable(msg.sender);
   }
 
+  /**
+  * @dev Give a Rose to the contract owner
+  * @param _name name of the rose giver!
+  * @param _message a fabulous message from the rose giver
+   */
+
+   function giveRose(string memory _name, string memory _message) public payable {
+
+    require(msg.value > 0, "A rose could cost you more than 0 eth!");
+
+    //Add the memo to storage!
+    memos.push(Memo(
+      msg.sender,
+      block.timestamp,
+      _name,
+      _message
+    ));
+
+   }
+
 }
